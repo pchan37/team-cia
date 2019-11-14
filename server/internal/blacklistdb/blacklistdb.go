db59cbe6cf5a46df8fc642a28ac13c91cb9f2012
Patrick Chan <patrickchan1999@gmail.com>
2019-11-14 16:08:32 -0500

    Add functions for adding and removing urls from the blacklist

---
+++ server/internal/blacklistdb/blacklistdb.go
@@ -0,0 +1,60 @@
+package blacklistdb
+
+import (
+	"context"
+	"time"
+
+	"github.com/pchan37/team-cia/server/internal/blacklistdb/blacklist"
+	"github.com/pchan37/team-cia/server/internal/config"
+	"github.com/pchan37/team-cia/server/internal/database"
+	"github.com/spf13/viper"
+)
+
+var (
+	dbClient = database.MustMongoClient(context.TODO(), "mongodb://localhost:27017")
+)
+
+func GetURLs() ([]blacklist.Entry, error) {
+	dbName := viper.GetString(config.DBName)
+	masterBlacklist := dbClient.Database(dbName).Collection("blacklist")
+
+	cursor, err := masterBlacklist.Find(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var urls []blacklist.Entry
+	for cursor.Next(context.TODO()) {
+		var u blacklist.Entry
+		if err := cursor.Decode(&u); err != nil {
+			return nil, err
+		}
+
+		urls = append(urls, u)
+	}
+	return urls, nil
+}
+
+func AddURL(url string) error {
+	dbName := viper.GetString(config.DBName)
+	masterBlacklist := dbClient.Database(dbName).Collection("blacklist")
+
+	now := time.Now()
+	entry := &blacklist.Entry{
+		URL:  url,
+		Time: now.Format("01-02-2006 15:04:05"),
+	}
+	_, err := masterBlacklist.InsertOne(context.TODO(), entry)
+	return err
+}
+
+func DeleteURL(url string) error {
+	dbName := viper.GetString(config.DBName)
+	masterBlacklist := dbClient.Database(dbName).Collection("blacklist")
+
+	entry := &blacklist.Entry{
+		URL: url,
+	}
+	_, err := masterBlacklist.DeleteOne(context.TODO(), entry)
+	return err
+}
