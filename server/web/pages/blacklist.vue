74e7953b5a7f5084b7590c250f60ae089010edbb
Patrick Chan <patrickchan1999@gmail.com>
2019-11-14 20:07:27 -0500

    Add a page for viewing blacklist

---
+++ server/web/pages/blacklist.vue
@@ -0,0 +1,57 @@
+<template>
+<section class="section">
+  <div class="container">
+    <h1 class="title">
+      Blacklist
+    </h1>
+
+    <b-table
+      v-if="this.statusType !== 'error'"
+      :data="data"
+      :columns="columns"
+      paginated
+      isPaginationSimple="false"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+      />
+    <b-message
+      v-else
+      >
+      {{ this.status }}
+    </b-message>
+  </div>
+</section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      data: [],
+      columns: [
+        {
+          field: 'url',
+          label: 'URL',
+          searchable: true,
+        },
+        {
+          field: 'time',
+          label: 'Time added',
+          searchable: true,
+        },
+      ],
+      status: '',
+      statusType: '',
+    };
+  },
+  asyncData() {
+    return axios.get('/get_blacklist')
+      .then((response) => ({ data: response.data }))
+      .catch((error) => error.response.data);
+  },
+};
+</script>
