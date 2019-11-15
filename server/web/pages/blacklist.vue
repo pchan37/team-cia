821282e0f98818bac459d54296f88859939a71c0
Patrick Chan <patrickchan1999@gmail.com>
2019-11-15 18:15:47 -0500

    Add navbar, restyle the interface

--- server/web/pages/blacklist.vue
+++ server/web/pages/blacklist.vue
@@ -1,7 +1,36 @@
 <template>
-<section class="section">
-  <div class="container">
-    <h1 class="title">
+<section class="hero is-info is-fullheight">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <span
+            class="navbar-burger burger"
+            @click.stop="isOpen = !isOpen"
+            v-bind:class="{ 'is-active': isOpen }"
+            >
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div class="navbar-menu" v-bind:class="{ 'is-active': isOpen }" :is-active="isOpen">
+          <div class="navbar-end">
+            <nuxt-link to="/" class="navbar-item">
+              Home
+            </nuxt-link>
+            <nuxt-link to="/docs" class="navbar-item">
+              Documentation
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="hero-body">
+    <div class="container has-white-background has-sm-padding is-rounded">
+    <h1 class="title has-black-text">
       Blacklist
     </h1>
 
@@ -9,8 +38,9 @@
       v-if="this.statusType !== 'error'"
       :data="data"
       :columns="columns"
+      :color="info"
       paginated
-      isPaginationSimple="false"
+      striped
       aria-next-label="Next page"
       aria-previous-label="Previous page"
       aria-page-label="Page"
@@ -22,6 +52,15 @@
       >
       {{ this.status }}
     </b-message>
+    </div>
+  </div>
+
+  <div class="hero-foot">
+    <nav class="tabs is-fullwidth">
+      <div class="container has-text-centered">
+        <span>&copy; {{ new Date().getFullYear() }} - Team CIA</span>
+      </div>
+    </nav>
   </div>
 </section>
 </template>
@@ -56,3 +95,22 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.pagination-link.is-current {
+    background-color: hsl(204, 86%, 53%); 
+    border-color: hsl(204, 86%, 53%);
+}
+.has-white-background {
+    background-color: white;
+}
+.has-black-text {
+    color: black !important;
+}
+.has-sm-padding {
+    padding: 10px;
+} 
+.is-rounded {
+    border-radius: 15px;
+}
+</style>
