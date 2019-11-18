d20dd049bb788e2a155b8424773a3add6a5a43f9
Patrick Chan <patrickchan1999@gmail.com>
2019-11-17 19:06:39 -0500

    Add login and register pages

---
+++ server/web/pages/login.vue
@@ -0,0 +1,72 @@
+<template>
+<section class="hero is-info is-fullheight">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <span
+            @click.stop="isOpen = !isOpen"
+            v-bind:class="{ 'is-active': isOpen }"
+            class="navbar-burger burger"
+            >
+            <span />
+            <span />
+            <span />
+          </span>
+        </div>
+        <div v-bind:class="{ 'is-active': isOpen }" :is-active="isOpen" class="navbar-menu">
+          <div class="navbar-end">
+            <nuxt-link to="/" class="navbar-item">
+              Home
+            </nuxt-link>
+            <nuxt-link to="/blacklist" class="navbar-item">
+              Blacklist
+            </nuxt-link>
+            <nuxt-link to="/docs" class="navbar-item">
+              Documentation
+            </nuxt-link>
+            <a v-if="username !== undefined" @click="logout" class="navbar-item">
+              Logout
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="hero-body">
+    <div class="container">
+    <div class="columns">
+      <div
+        class="column is-mobile is-two-thirds-mobile is-offset-2-mobile \
+               is-half-desktop is-offset-one-quarter-desktop"
+        >
+        <login-form successful-login-link="/dev_console" successful-login-force-load="true" />
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <div class="hero-foot">
+    <nav class="tabs is-fullwidth">
+      <div class="container has-text-centered">
+        <span>&copy; {{ new Date().getFullYear() }} - Team CIA</span>
+      </div>
+    </nav>
+  </div>
+</section>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue';
+
+export default {
+  name: 'Login',
+  components: { LoginForm },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+};
+</script>
