dad75f1610de4ce10f90df4e0dda9d9fb542c489
jessiclin <linje1202@gmail.com>
2019-11-22 15:40:47 -0500

    Removed convertToBinary function

--- extension/background.js
+++ extension/background.js
@@ -111,19 +111,6 @@ function captureTab() {
   );
 }
 
-function convertDataURIToBinary(dataURI) {
-  let BASE64_MARKER = ";base64,";
-  let base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-  let base64 = dataURI.substring(base64Index);
-  let raw = window.atob(base64);
-  let rawLength = raw.length;
-  let array = new Uint8Array(new ArrayBuffer(rawLength));
-
-  for (let i = 0; i < rawLength; i++) array[i] = raw.charCodeAt(i);
-
-  return array;
-}
-
 /******************************************************************/
 /***************** COMPARISON CODE STARTS HERE ********************/
 /******************************************************************/
